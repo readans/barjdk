1d98e6105dbae92b430cc4bc431445e4dc565ea6
Dylan Steven Ariza Rueda <dylan.ariza@novabase.com.co>
2023-09-11 23:37:29 -0500

    first commit

---
+++ inventory-services/src/main/java/org/barjdk/controllers/HelloWorldController.java
@@ -0,0 +1,13 @@
+package org.barjdk.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, world!";
+    }
+}
